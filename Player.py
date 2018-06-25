b5d20bc7675d3a92c1a617aa6d6d194a8746c475
v-martins <vmam@cin.ufpe.br>
2018-06-25 16:09:50 -0300

    Started p2p functions

--- Player.py
+++ Player.py
@@ -6,3 +6,11 @@ class Player:
 
     def getPlayerInfo(self):
         return ("ID: "+str(self.id)+"    IP: "+str(self.ip)+"    PORT: "+str(self.port))
+
+
+class inGamePlayer(Player):
+    def __init__(self, id, ip):
+        self.id = id
+        self.ip = ip
+        self.port = None
+        self.value = None
--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+from Player import inGamePlayer
 
 HOST = '172.22.79.112'
 PORT = 5000
@@ -13,19 +14,55 @@ def choseNumber():
         number = input()
     return number
 
-def getIPs(info, peers):
-    ip = []
+def getPlayers(info, peers):
+    allPlayers = []
     j = 1
     while (j <= peers):
-        i = info[j].find("IP: ") + len("IP: ")
+        i = info[j].find("ID: ") + len("ID: ")
         str1 = ''
         while(info[j][i] != ' '):
             str1 += info[j][i]
             i += 1
-        print(str1)
+        i = info[j].find("IP: ") + len("IP: ")
+        str2 = ''
+        while(info[j][i] != ' '):
+            str2 += info[j][i]
+            i += 1
+
+        allPlayers.append(inGamePlayer(str1, str2))
         j += 1
-        ip.append(str1)
-    return ip
+        
+    return allPlayers
+
+def getIP(info):
+    i = info.find("IP: ") + len("IP: ")
+    str1 = ''
+    while(info[i] != ' '):
+        str1 += info[i]
+        i += 1
+    return str1
+
+PORT_RCV_VALUE = 31245
+KNOWN_VALUES = 1
+MY_IP = None
+valueByIP = []
+
+def enviarValor(valor, ips):
+    msg = str(valor)
+    for i in ips:
+        if (i != MY_IP):
+            destPlayer = (i, PORT_RCV_VALUE)
+            udp.sendto(msg.encode('utf-8'), destPlayer)
+
+def receberValor():
+    global KNOWN_VALUES
+    while (KNOWN_VALUES < PEERS):
+        valor, address = udp.recvfrom(2048)
+        ip, port = address
+        if (valueByIP[ip] == None):
+            valueByIP[ip] = int(valor)
+            KNOWN_VALUES += 1
+
 
 print("Deseja entrar no jogo? (Y / N)")
 msg = input()
@@ -34,18 +71,22 @@ if (msg == 'Y'):
     udp.sendto(msg.encode(), dest)
 
 while True:
+    
     serverMsg, serverAddress = udp.recvfrom(2048)
     msg = serverMsg.decode()
     print(msg)
     PEERS = int(msg[msg.find("Jogadores necessários: ") + len("Jogadores necessários: ")])
     info = msg.split('\n')
+
+    if(MY_IP == None):
+        MY_IP = getIP(info[len(info)-2])
     
     if (len(info) - 2 == PEERS):
         print("Link Start")
-        ips = getIPs(info, PEERS)
-        n = choseNumber()
-        print(n)
-        # Enviar a todos os IP's
+        allPlayers = getPlayers(info, PEERS)
+        valueByIP[MY_IP] = choseNumber()
+        # Envia o seu valor aos outros jogadores
+        # Recebe os valores dos outros jogadores
 
 input()
 udp.close()
